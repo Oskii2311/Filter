978976aa9841724bc25cae80a52e26a46f0ea0c6
Oskii2311 <os.janczak@gmail.com>
2018-08-23 11:30:17 +0200

    filter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
+import { FilterService } from './filter.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Filter';
-}
+export class AppComponent {}
---
+++ src/app/filter.service.ts
@@ -0,0 +1,98 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FilterService {
+  private previousFilter = [];
+  constructor() {}
+
+  filter(data: any[], value?: string, target?: string) {
+    if (value) {
+      if (!target) {
+        target = 'global';
+      }
+      this.previousFilter = this.addNewFilters(value, target);
+    }
+
+    if (!this.isValueExist(value)) {
+      this.previousFilter = this.removeFilter(target);
+
+      if (this.previousFilter.length === 0) {
+        return data;
+      }
+    }
+
+    this.previousFilter.forEach(currentFilter => {
+      if (currentFilter.target === 'global') {
+        data = this.globalFilter(data, currentFilter.value);
+      } else {
+        data = this.specyficKeyFilter(data, currentFilter);
+      }
+    });
+
+    return data;
+  }
+
+  private specyficKeyFilter(data: any[], currentFilter: any) {
+    return data.filter(el => {
+      if (this.isIncludeValue(el, currentFilter)) {
+        return el;
+      }
+    });
+  }
+
+  private globalFilter(data: any[], currentFilterValue: string) {
+    return data.filter(el => this.returnFilteredData(el, currentFilterValue));
+  }
+
+  private returnFilteredData(el: any, currentFilterValue: string) {
+    const keys = Object.keys(el);
+
+    return keys.find(key => this.findValue(key, currentFilterValue, el));
+  }
+
+  private findValue(key: string, currentFilterValue: string, el: any) {
+    if (typeof el[key] === 'string') {
+      if (el[key].toLowerCase().includes(currentFilterValue.toLowerCase())) {
+        return el;
+      }
+    }
+  }
+
+  private isIncludeValue(el, filter) {
+    if (el[filter.target].toLowerCase().includes(filter.value.toLowerCase())) {
+      return true;
+    }
+
+    return false;
+  }
+
+  private addNewFilters(value: string, target: string) {
+    this.previousFilter = this.previousFilter.filter(el => {
+      if (el.target !== target) {
+        return el;
+      }
+    });
+
+    this.previousFilter.push({ value, target });
+
+    return this.previousFilter;
+  }
+
+  private removeFilter(target: string) {
+    return this.previousFilter.filter(el => {
+      if (el.target !== target) {
+        return el;
+      }
+    });
+  }
+
+  private isValueExist(value: any) {
+    if (!value || value === undefined || value === '') {
+      return false;
+    }
+
+    return true;
+  }
+}
